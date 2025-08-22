b02d9282d4ad546f86d3edca1f8d84de55b23b38
Achal Bhardwaj <achalbhardwaj17@gmail.com>
2025-08-22 16:25:23 +0530

    Add files via upload

---
+++ checks/env_check.py
@@ -0,0 +1,9 @@
+# Quick environment sanity check
+import sys, platform
+import numpy as np
+import pandas as pd
+
+print("Python:", sys.version)
+print("Platform:", platform.platform())
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
